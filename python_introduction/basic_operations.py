b5d1cf2c4ec1dc2d1fec60e62d85f9237aec0cdb
Tracy Tenkorang <tenkorangtracy200@gmail.com>
2024-06-23 13:50:37 +0000

    added new file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+subtract = number1 - number2
+product = number1 * number2
+
+
+print ("addition of", number1, "and", number2, "is", sum)
+print("Subtraction of 10 and 5 is", subtract)
+print("Product of 10 and 5 is", product)
