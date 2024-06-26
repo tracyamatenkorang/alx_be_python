91e9591fdd2324846a81cb19c57792accac8fffc
Tracy Tenkorang <tenkorangtracy200@gmail.com>
2024-06-26 16:10:19 +0000

    my weather

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,17 @@
+weather = input("What's the weather like today? (sunny/rainy/cold):") . lower()
+
+if weather == "sunny":
+    recommendation = "Wear a t-shirt and sunglasses."
+elif weather == "rainy":
+    recommendation = "Don't forget your umbrella and a raincoat."
+
+elif weather == "cold":
+    recommendation = "Make sure to wear a warm coat and a scarf."
+
+else:
+    recommendation = "Sorry, I don't have recommendations for this weather."
+
+
+print(recommendation)
+
+
