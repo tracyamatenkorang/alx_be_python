41bc75d64b1bf657c8a24cbbb5971220fd805fac
Tracy Tenkorang <tenkorangtracy200@gmail.com>
2024-06-30 21:08:15 +0000

    my match file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,7 +1,7 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-operation = str(input("Choose the operation"))
+operation = str(input("Choose the operation (+, -, *, /): "))
 
 match operation:
     case ('+'):
